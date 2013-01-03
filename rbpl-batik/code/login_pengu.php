574c030c1c598d7a3f7a71abb9550f9c3739adc3
as.redbios <as.redbios@gmail.com>
2013-01-03 23:39:22 +0000

    ini hasil pembaruan ku setalah liburan menambah halaman login buat pengguna, tpi masih ada beberapa yang error,masih garus diperbaiki

---
+++ rbpl-batik/code/login_pengu.php
@@ -0,0 +1,21 @@
+<?php
+include "config.php";
+
+$login = $_POST['login'];
+$pass = $_POST['password'];
+
+$sql = mysql_query("SELECT * FROM pengguna WHERE Id_login_peng='".$login."' AND password='".$pass."'");
+$hasil = mysql_num_rows($sql);
+
+if ($hasil > 0 ){
+	session_start();
+	$_SESSION['login'];
+	header('location=../view/home_pengu.php');
+}else{
+	echo "<script>alert('Login gagal, silahkan coba lagi ..');window.history.go(-1)</script>";
+	header('location=../view/login_pengu.php');
+}
+
+/*echo "<script>alert('Selamat anda sudah berhasil terdaftar, Selamat Datang di KOBATPAM ;) ');window.history.go(1)</script>";*/
+/*echo "<script>window.location='../view/daftar_pengunjung_next.php'</script>";*/
+?>
