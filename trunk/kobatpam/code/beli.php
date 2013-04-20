9ec1cc2d2ec6bb441787c01bb2844228add1cc33
as.redbios@gmail.com <as.redbios@gmail.com@8d7dd34d-fb03-0f02-970a-db19c0e13a3b>
2013-04-20 04:28:10 +0000

    Perbaikan bug pada beberapa field dan tanggal pembelian

--- trunk/kobatpam/code/beli.php
+++ trunk/kobatpam/code/beli.php
@@ -3,7 +3,7 @@ include "config.php";
 session_start();
 
 $jml = $_POST['jml'];
-$tgl = $_POST['tgl'];//date(‘Y-m-d’);
+$tgl = date('Y-m-d');//$_POST['tgl'];//date(‘Y-m-d’);
 
 $sql = mysql_query("SELECT * FROM produk WHERE id_produk='".$_SESSION['id']."'");
 while($fet = mysql_fetch_array($sql)){
@@ -44,10 +44,10 @@ $totalpembelian = $harga * $jml;
 $notif_peng = "Anda telah membeli ".$nama." sebanyak ".$jml." dari ".$nama_ud." dengan total pembelian RP".$totalpembelian;
 $notif_ud = "Produk dari perusahaan anda dengan nama ".$nama.", telah dibeli oleh ".$nama_user." sebanyak ".$jml." dengan total pembelian RP".$totalpembelian;
 
-//insert memebeli
+//insert membeli
 mysql_query("INSERT INTO `membeli`(`id_penj`, `id_user`, `namabarang`, `Jumlahbarang`, `totalharga`, `tanggal`) VALUES ('".$inv1."','".$id_user."','".$nama."','".$jml."','".$totalpembelian."','".$tgl."')");
 //insert notif pengguna
-mysql_query("INSERT INTO `notif_peng`(`id_notif_2`, `id_user`, `isi_notif_2`, `tgl_2`, `status_2`) VALUES ('".$inv1."',.".$id_user."','".$notif_peng."','".$tgl."','0')");	
+mysql_query("INSERT INTO `notif_peng`(`id_notif_2`, `id_user`, `isi_notif_2`, `tgl_2`, `status_2`) VALUES ('".$inv1."',.".$id_user."','".$notif_peng."','".$tgl."','0')");
 //mengisi notif ud
 mysql_query("INSERT INTO `notif_ud`(`id_notif_1`, `id_dagang`, `isi_notif_1`, `tgl_1`, `status_1`) VALUES ('".$inv2."','".$_SESSION['idd']."','".$notif_ud."','".$tgl."','0')");
 header('location: ../view/home_utama_login.php');
