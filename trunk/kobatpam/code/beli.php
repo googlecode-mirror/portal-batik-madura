f595fe9e110b4016eb40df62921b3a043633983c
as.redbios@gmail.com <as.redbios@gmail.com@8d7dd34d-fb03-0f02-970a-db19c0e13a3b>
2013-05-22 17:37:51 +0000

    pengisian notifikasi dan update stok setelah pembelian sudah beres .... :) tinggal message saat pembeli berhasil membeli barang

--- trunk/kobatpam/code/beli.php
+++ trunk/kobatpam/code/beli.php
@@ -14,13 +14,15 @@ while($fet = mysql_fetch_array($sql)){
 	$gambar = $fet['Gambar'];
 }
 
+//id pada tabel membeli
 $id_lam4 = mysql_result(mysql_query("SELECT MAX(id_penj) FROM membeli"),0);
 if ($id_lam4 == 0){
-    $inv1 = 501;
+    $inv3 = 501;
 }else{
-    $inv1 = $id_lam4+1;
+    $inv3 = $id_lam4+1;
 }
 
+//id pada tabel notif_peng
 $id_lam1 = mysql_result(mysql_query("SELECT MAX(id_notif_2) FROM notif_peng"),0);
 if ($id_lam1 == 0){
     $inv1 = 301;
@@ -28,6 +30,7 @@ if ($id_lam1 == 0){
     $inv1 = $id_lam1+1;
 }
 
+//id pada tabel notif_ud
 $id_lam2 = mysql_result(mysql_query("SELECT MAX(id_notif_1) FROM notif_ud"),0);
 if ($id_lam2 == 0){
     $inv2 = 401;
@@ -41,15 +44,21 @@ $nama_user = mysql_result(mysql_query("SELECT `nama_user` FROM `pengguna` WHERE 
 
 $totalpembelian = $harga * $jml;
 
+//jumlah produk awal
+$stok_akhir = $stok - $jml;
+
 $notif_peng = "Anda telah membeli ".$nama." sebanyak ".$jml." dari ".$nama_ud." dengan total pembelian RP".$totalpembelian;
 $notif_ud = "Produk dari perusahaan anda dengan nama ".$nama.", telah dibeli oleh ".$nama_user." sebanyak ".$jml." dengan total pembelian RP".$totalpembelian;
 
 //insert membeli
-mysql_query("INSERT INTO `membeli`(`id_penj`, `id_user`, `namabarang`, `Jumlahbarang`, `totalharga`, `tanggal`) VALUES ('".$inv1."','".$id_user."','".$nama."','".$jml."','".$totalpembelian."','".$tgl."')");
+mysql_query("INSERT INTO `membeli`(`id_penj`, `id_user`, `namabarang`, `Jumlahbarang`, `totalharga`, `tanggal`) VALUES ('".$inv3."','".$id_user."','".$nama."','".$jml."','".$totalpembelian."','".$tgl."')");
 //insert notif pengguna
-mysql_query("INSERT INTO `notif_peng`(`id_notif_2`, `id_user`, `isi_notif_2`, `tgl_2`, `status_2`) VALUES ('".$inv1."',.".$id_user."','".$notif_peng."','".$tgl."','0')");
+mysql_query("INSERT INTO `notif_peng`(`id_notif_2`, `id_user`, `isi_notif_2`, `tgl_2`, `status_2`) VALUES ('".$inv1."','".$id_user."','".$notif_peng."','".$tgl."','0')");
 //mengisi notif ud
 mysql_query("INSERT INTO `notif_ud`(`id_notif_1`, `id_dagang`, `isi_notif_1`, `tgl_1`, `status_1`) VALUES ('".$inv2."','".$_SESSION['idd']."','".$notif_ud."','".$tgl."','0')");
+//update jumlah stok
+mysql_query("UPDATE `produk` SET `stok_produk` = '".$stok_akhir."' WHERE id_produk='".$_SESSION['id']."'");
+/*echo "<script>alert('Pembelian telah berhasil, silahkan cek notifikasi anda');window.history.go(1)</script>";*/
 header('location: ../view/home_utama_login.php');
 
 ?>
